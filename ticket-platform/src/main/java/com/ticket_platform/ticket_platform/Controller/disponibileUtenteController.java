16f2c55411f4e1f38a656c620eaa1bd7f49df903
federicomolino <federicomolino450@gmail.com>
2025-05-15 23:57:31 +0200

    fix: puoi modificare la disponibilità solo se hai tutti i ticket in COMPLETATO

--- ticket-platform/src/main/java/com/ticket_platform/ticket_platform/Controller/disponibileUtenteController.java
+++ ticket-platform/src/main/java/com/ticket_platform/ticket_platform/Controller/disponibileUtenteController.java
@@ -1,11 +1,13 @@
 package com.ticket_platform.ticket_platform.Controller;
 
+import com.ticket_platform.ticket_platform.Repository.ticketRepository;
 import com.ticket_platform.ticket_platform.Repository.utenteRepository;
 import com.ticket_platform.ticket_platform.Service.disponibileUtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 
@@ -18,11 +20,26 @@ public class disponibileUtenteController {
     @Autowired
     private disponibileUtenteService disponibileUtenteService;
 
+    @Autowired
+    private ticketRepository ticketRepository;
+
     @PostMapping("/path")
-    public String gestioneDisponibilitaUtente( @RequestParam(value = "disponibilitaUtente",required = false, defaultValue = "false")
-                                                   boolean checkDisponibile,
-                                               Principal principal){
-        disponibileUtenteService.disponibilitaUtente(checkDisponibile,principal);
+    public String gestioneDisponibilitaUtente(@RequestParam(value = "disponibilitaUtente",required = false, defaultValue = "false")
+                                               boolean checkDisponibile,
+                                              Principal principal, RedirectAttributes redirectAttributes){
+//        String username = principal.getName();
+//        Utente utente = utenteRepository.findByUsername(username).get();
+//        Ticket ticket = ticketRepository.findById(utente.getIdUtente()).get();
+//        if (ticket.getStato().equals("DA_FARE") || ticket.getStato().equals("IN_CORSO")){
+//            redirectAttributes.addFlashAttribute("erroreDisponibilita",
+//                    "Non puoi modificare la disponibilità con ticket attivi.");
+//        }
+
+        boolean disponibileUtente = disponibileUtenteService.disponibilitaUtente(checkDisponibile,principal);
+        if(!disponibileUtente){
+            redirectAttributes.addFlashAttribute("erroreDisponibilita",
+                    "Non puoi modificare la disponibilità, ticket attivi.");
+        }
         return "redirect:/";
     }
 }
--- ticket-platform/src/main/java/com/ticket_platform/ticket_platform/Service/disponibileUtenteService.java
+++ ticket-platform/src/main/java/com/ticket_platform/ticket_platform/Service/disponibileUtenteService.java
@@ -1,11 +1,14 @@
 package com.ticket_platform.ticket_platform.Service;
 
+import com.ticket_platform.ticket_platform.Entity.Ticket;
 import com.ticket_platform.ticket_platform.Entity.Utente;
+import com.ticket_platform.ticket_platform.Repository.ticketRepository;
 import com.ticket_platform.ticket_platform.Repository.utenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,6 +16,9 @@ public class disponibileUtenteService {
 
     @Autowired
     private utenteRepository utenteRepository;
+
+    @Autowired
+    private ticketRepository ticketRepository;
 
     //recupera utente
     public Optional<Utente> recuperoUtente(Principal principal){
@@ -22,13 +28,20 @@ public class disponibileUtenteService {
     }
 
     //check disponibilità
-    public void disponibilitaUtente(boolean checkDisponibile, Principal principal){
-        String username = principal.getName();
-        Optional<Utente> utente = utenteRepository.findByUsername(username);
-        if (utente.isPresent()){
-            Utente usernameUtente = utente.get();
-            usernameUtente.setDisponibile(checkDisponibile);
-            utenteRepository.save(usernameUtente);
+    public boolean disponibilitaUtente(boolean checkDisponibile, Principal principal){
+        Optional<Utente> utente = recuperoUtente(principal);
+
+        Utente utenteLoggato = utente.get();
+        List<Ticket> tickets = ticketRepository.findByUtente(utenteLoggato);
+        for (Ticket ticket : tickets){
+            if (ticket.getStato().equals("DA_FARE") || ticket.getStato().equals("IN_CORSO")){
+                return false;
+            }
         }
+
+        utenteLoggato.setDisponibile(checkDisponibile);
+        utenteRepository.save(utenteLoggato);
+
+        return true;
     }
 }
